d59f0dde82ab0d157c2ea21e8cc7b83836ac600e
Danca123 <bognardaniel12@gmail.com>
2025-11-30 09:38:23 +0100

    welcome üzenet módosítva

--- WebApp/obj/Debug/net7.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net7.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09e2730e224acdabbaba72cdcae93f3d5deba6e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d0e179728c8160d9083e89ffd879cba8c6c4461")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
